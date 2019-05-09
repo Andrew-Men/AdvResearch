572172af31121f1d3f19b3a9df4d8ff615fca4df
Eis <eis@BALTHASAR.local>
2019-05-09 19:06:35 +0800

    让代码更整洁了，多了三个输出文件
    三个新增输出文件分别为：
    转换坐标系后的标记表格
    用.npy储存的图像数据
    用.mat储存的图像数据

--- python/data_preprocessing/find_the_pic.py
+++ python/data_preprocessing/find_the_pic.py
@@ -7,8 +7,9 @@ import SimpleITK as sitk
 import numpy as np
 import csv
 import os
-from glob import glob
+import glob
 import pandas as pd
+import scipy.io
 
 # FIX CRASH #
 import matplotlib
@@ -16,21 +17,32 @@ matplotlib.use("TkAgg")
 # --------- #
 import matplotlib.pyplot as plt
 
+# 尝试调用 tqdm 如果失败就用一个lambda表达式代替
 try:
     from tqdm import tqdm # long waits are not fun
 except:
     print('TQDM does make much nicer wait bars...')
     tqdm = lambda x: x
 
+# 定义工作目录和文件位置
 working_dir = os.path.split(os.path.realpath(__file__))[0]
-luna_subset_path = os.path.join(working_dir, "subset0")
+luna_subset_path = "/Users/eis/Desktop/subset0"
+annotations_path = os.path.join(working_dir, "annotations.csv")
 output_path = os.path.join(working_dir, "output")
-file_list=glob(os.path.join(luna_subset_path,"*.mhd"))
+file_list=glob.glob(os.path.join(luna_subset_path,"*.mhd"))
 
-#####################
-#
-# Helper function to get rows in data frame associated 
+# 遗留函数，作用未知
+def matrix2int16(matrix):
+    '''
+    matrix must be a numpy array NXN
+    Returns uint16 version
+    '''
+    m_min= np.min(matrix)
+    m_max= np.max(matrix)
+    matrix = matrix-m_min
+    return(np.array(np.rint( (matrix-m_min)/float(m_max-m_min) * 65535.0),dtype=np.uint16))
 
+# 遗留函数，产生一个512x512的遮罩矩阵，其中要显示的区域值为1，其他为0
 def make_mask(center,diam,z,width,height,spacing,origin):
     '''
     Center : centers of circles px -- list of coordinates x,y,z
@@ -82,13 +94,14 @@ def make_mask(center,diam,z,width,height,spacing,origin):
 
     return mask
 
+# 返回一个有三个元素的tuple，前两个元素是要切割区域的左上角点的矩阵索引位置(y,x)
+# 第三个元素是一个标志位，用于警告上级函数nodule是否不在图片中心（因为太靠边缘导致）
 def get_mask_position(center,diam,z,width,height,spacing,origin):
     SIZE = 50 # 数据切片大小 50*50
     ORIGIN_SIZE = 512
     notInTheCenterFlag = 0
     if diam/spacing[0] > SIZE:
-        print("this nodule is bigger than mask size!")
-        return mask
+        print("this nodule is bigger than 50x50 size!")
 
     v_center = (center-origin)/spacing
     v_xmin = int(v_center[0])-SIZE//2 + 1 # 加一保证 size 为 50x50
@@ -104,18 +117,7 @@ def get_mask_position(center,diam,z,width,height,spacing,origin):
     v_xmin = ub if v_xmin > ub else v_xmin
     v_ymin = ub if v_ymin > ub else v_ymin
 
-    return (v_xmin,v_ymin,notInTheCenterFlag)
-
-
-def matrix2int16(matrix):
-    '''
-    matrix must be a numpy array NXN
-    Returns uint16 version
-    '''
-    m_min= np.min(matrix)
-    m_max= np.max(matrix)
-    matrix = matrix-m_min
-    return(np.array(np.rint( (matrix-m_min)/float(m_max-m_min) * 65535.0),dtype=np.uint16))
+    return (v_ymin,v_xmin,notInTheCenterFlag)
 
 # with each file
 def get_filename(case):
@@ -123,16 +125,16 @@ def get_filename(case):
     for f in file_list:
         if case in f:
             return(f)
-#####
-#
-# The locations of the nodes
-df_node = pd.read_csv(os.path.join(working_dir, "annotations.csv"))
+
+# Get locations of the nodes
+df_node = pd.read_csv(annotations_path)
 df_node["file"] = df_node["seriesuid"].map(lambda file_name: get_filename(file_name))
 df_node = df_node.dropna()
 
-#####
-#
+
 # Looping over the image files
+sml_img_data = []
+v_axis_annotation_data = [] # 用于储存新的坐标系下的标记数据
 for fcount, img_file in enumerate(tqdm(file_list)):
     mini_df = df_node[df_node["file"]==img_file] #get all nodules associate with file
     if mini_df.shape[0]>0: # some files may not have a nodule--skipping those 
@@ -148,23 +150,37 @@ for fcount, img_file in enumerate(tqdm(file_list)):
             node_y = cur_row["coordY"]
             node_z = cur_row["coordZ"]
             diam = cur_row["diameter_mm"]
-            # just keep 3 slices
+            seriesuid = cur_row["seriesuid"]
+            # keep 3 slices
             imgs = np.ndarray([3,height,width],dtype=np.int16)
+            sml_imgs = np.ndarray([3,50,50],dtype=np.int16)
             center = np.array([node_x, node_y, node_z])   # nodule center
             v_center = np.rint((center-origin)/spacing)  # nodule center in voxel space (still x,y,z ordering)
-            for i, i_z in enumerate(np.arange(int(v_center[2])-1,
-                             int(v_center[2])+2).clip(0, num_z-1)): # clip prevents going out of bounds in Z
-                mask = get_mask_position(center, diam, i_z*spacing[2]+origin[2],
-                                 width, height, spacing, origin)
+
+
+            annotation = [seriesuid]
+            annotation.extend(v_center.tolist())
+            annotation.append(diam)
+            v_axis_annotation_data.append(annotation)
+
+            for i, i_z in enumerate(np.arange(int(v_center[2])-1, int(v_center[2])+2).clip(0, num_z-1)): # clip prevents going out of bounds in Z
+                mask = get_mask_position(center, diam, i_z*spacing[2]+origin[2], width, height, spacing, origin)
                 temp_img = img_array[i_z]
+                small_img = temp_img[mask[0]:mask[0]+50,mask[1]:mask[1]+50] # nodule 50x50图片
                 imgs[i] = temp_img
-                small_img = temp_img[mask[1]:mask[1]+49,mask[0]:mask[0]+49] # nodule 50x50图片
+                sml_imgs[i] = small_img
                 if mask[2] == 1:
-                    print("noudle 不在图片的中心！")
-                fig = plt.figure()
-                plt.subplot(2,1,1)
-                plt.imshow(small_img,cmap='gray')
-                plt.subplot(2,1,2)
-                plt.imshow(temp_img,cmap='gray')
-                plt.show()
-                exit()
+                    print("nodule 不在图片的中心！")
+            sml_img_data.append(sml_imgs)
+
+# 将切出的数据存储到 .npy 和 .mat中，方便后续使用
+sml_img_data = np.asarray(sml_img_data)
+np.save(os.path.join(output_path, 'sml_img_data'), sml_img_data)
+scipy.io.savemat(os.path.join(output_path, 'sml_img_data.mat'), {'sml_img_data': sml_img_data})
+
+# 将转换坐标系后的标记存储到 annotation_v.csv
+with open(os.path.join(working_dir,'output','annotation_v.csv'), 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerow(["seriesuid","coordX","coordY","coordZ","diameter_mm"])
+    writer.writerows(v_axis_annotation_data)
+csvFile.close()
