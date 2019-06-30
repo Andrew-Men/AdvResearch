5345bb7bd20151d602d4803c60e04a8d9aad6303
Eis <eis@BALTHASAR-6.local>
2019-06-30 16:44:05 +0800

    增加输出512x512图片的代码
    默认为注释状态

--- data/saveimage.py
+++ data/saveimage.py
@@ -8,5 +8,5 @@ pos_data = np.load(os.path.join(working_dir,'positive_data.npy'))
 neg_data = np.load(os.path.join(working_dir,'negitive_data.npy'))
 
 for i in range(40):
-    imageio.imwrite(os.path.join(working_dir,'PNG','positive_{}.png'.format(i)).format(i),pos_data[i][1])
-    imageio.imwrite(os.path.join(working_dir,'PNG','negitive_{}.png'.format(i)).format(i),neg_data[i][1])
+    imageio.imwrite(os.path.join(working_dir,'PNG','positive_{}.png'.format(i)),pos_data[i][1])
+    imageio.imwrite(os.path.join(working_dir,'PNG','negitive_{}.png'.format(i)),neg_data[i][1])
